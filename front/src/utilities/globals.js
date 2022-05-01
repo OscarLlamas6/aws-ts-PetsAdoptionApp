bd934c28d24e47dd23a9a4a408090937b9c86af7
201709309 <josesantizo149@gmail.com>
2022-05-01 00:30:41 -0600

    ultimos cambios para release final

--- front/src/utilities/globals.js
+++ front/src/utilities/globals.js
@@ -1,7 +1,8 @@
 let variables = {
     host: '3.21.163.103',
     puerto: '3006',
-    enlace: `http://localhost:4000`
+    // enlace: `http://localhost:4000`,
+    enlace: `http://semi1balancer-08f941dd98328774.elb.us-east-1.amazonaws.com`,
 }
 
 export default variables
